c063905812048ae9b5f06e684de4ea3e31a71f6a
rnwhyu <r_wahyuningtyas@yahoo.co.id>
2023-07-27 15:06:58 +0700

    add migration postgre file

---
+++ migrations/1690359199674_create-table-songs.js
@@ -0,0 +1,40 @@
+/* eslint-disable camelcase */
+
+exports.up = (pgm) => {
+  pgm.createTable('songs', {
+    id: {
+      type: 'Varchar(50)',
+      primaryKey: true,
+    },
+    title: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    year: {
+      type: 'SMALLINT',
+      notNull: true,
+    },
+    genre: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    performer: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    duration: {
+      type: 'INT',
+      notNull: false,
+    },
+    albumId: {
+      type: 'VARCHAR(50)',
+      notNull: false,
+    },
+  });
+  pgm.sql(`
+  ALTER TABLE songs ADD CONSTRAINT albumId FOREIGN KEY (id) REFERENCES albums(id);`);
+};
+
+exports.down = (pgm) => {
+  pgm.dropTable('songs');
+};
