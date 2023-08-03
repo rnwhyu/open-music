92e2246b651400477e52fa98aac1d44c247a2663
rnwhyu <r_wahyuningtyas@yahoo.co.id>
2023-08-03 23:16:41 +0700

    remove debug code

--- src/services/postgres/AlbumsService.js
+++ src/services/postgres/AlbumsService.js
@@ -39,14 +39,8 @@ class AlbumService {
       text: 'SELECT id, title, performer FROM songs WHERE album_id = $1',
       values: [id],
     };
-    try {
-
-      const result = await this._pool.query(query);
-      return result.rows;
-    } catch (e){
-      console.log(e)
-      throw e
-    }
+    const result = await this._pool.query(query);
+    return result.rows;
   }
 
   async editAlbumById(id, { name, year }) {
